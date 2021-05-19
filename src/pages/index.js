c3f20058b202005d93c5eec438b519f3308e5551
Gianluca Belvisi <popippo@gmail.com>
2021-05-19 07:02:47 +0200

    Removed trips

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import React from "react"
 import Layout from "../components/Layout"
 import Seo from "../components/Seo"
 import Hero from "../components/Hero";
-import Trips from "../components/Trips";
 import Testimonials from "../components/Testimonials";
 import Stats from "../components/Stats";
 import Email from "../components/Email";
@@ -12,7 +11,6 @@ const Index = () => (
     <Layout>
         <Seo title="Home"/>
         <Hero/>
-        <Trips heading="My favorite stuff"/>
         <Testimonials/>
         <Stats/>
         <Email/>
