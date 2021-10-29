aec41a26d28f9fe39a68d5c72b14203d436d7a5c
Gianluca Belvisi <popippo@gmail.com>
2021-10-29 09:51:58 +0200

    Spoiler component

---
+++ src/components/Spoiler.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import styled from "styled-components";
+
+
+const Spoiler = ({children}) => {
+
+    const [isVisible, setIsVisible] = useState(false)
+    const toggleVisibility = () => {
+        setIsVisible(!isVisible)
+    }
+
+    return (
+        <Wrapper>
+            <Cover onClick={e => toggleVisibility()} isVisible={isVisible}/>
+
+            <Content>
+                {children}
+            </Content>
+        </Wrapper>
+    );
+};
+
+const Wrapper = styled.div`
+  position: relative;
+`
+
+const Cover = styled.label`
+  position: absolute;
+  cursor: pointer;
+  background-color: ${props => props.theme.button.mainColor};
+  opacity: ${({isVisible}) => (isVisible ? "0" : "1")};
+  height: 100%;
+  width: 100%;
+  transition: opacity .5s ease-in-out;
+  border-radius: 20px;
+  
+  &:after {
+    content: 'Spoiler. Click to reveal!';
+    position: absolute;
+    color: white;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+  }
+`
+
+const Content = styled.div`
+  padding: 1rem;
+`
+
+export default Spoiler;
