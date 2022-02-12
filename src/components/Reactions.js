95aa7633bd0e25a93adc9df73d65ab64c244b945
Gianluca Belvisi <popippo@gmail.com>
2022-02-12 19:24:00 +0100

    Reactions 1.0

--- src/components/Reactions.js
+++ src/components/Reactions.js
@@ -7,6 +7,7 @@ import {reactionData} from "../data/ReactionData";
 
 const Reactions = ({id}) => {
 
+    // reactions
     const [reactions, setReactions] = useState('{}');
 
     const increaseCount = (name) => {
@@ -27,7 +28,8 @@ const Reactions = ({id}) => {
         })
     }
 
-    useEffect( () => {
+
+    useEffect(() => {
         async function fetchData() {
             const database = getDatabase(app)
             const reactionRef = ref(database, 'reactions/' + id)
@@ -37,34 +39,40 @@ const Reactions = ({id}) => {
                 return stored
             })
         }
+
         fetchData()
     }, [id]);
-
-    function getCount(name) {
-        if (reactions == null)
-            return 0
-        const reaction = reactions[name]
-        return (reaction === undefined) ? 0 : reaction
-    }
 
     return (
         <Wrapper>
             <ReactTooltip effect="solid" backgroundColor="#ff9664"/>
             {reactionData.map((item, key) => (
-                <EmoteBox key={key} data-tip={item.tooltip} data-place="bottom" onClick = {e => increaseCount(item.name)}>
+                <EmoteBox key={key} data-tip={item.tooltip} data-place="bottom"
+                          onClick={e => increaseCount(item.name)}
+                >
                     <Emote>
                         <DisplayEmote>
                             {item.icon}
                         </DisplayEmote>
                     </Emote>
                     <Count>
-                        {getCount(item.name)}
+                        {reactions == null ? 0 : reactions[item.name] ?? 0}
                     </Count>
                 </EmoteBox>
             ))}
         </Wrapper>
     );
 };
+
+const NoSelect = styled.div`
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none;
+`
+
 const pulse = keyframes`
   from {
     transform: scale(1.4);
@@ -76,19 +84,19 @@ const pulse = keyframes`
 const Wrapper = styled.div`
   display: flex;
 `
-const Emote = styled.div`
+const Emote = styled(NoSelect)`
   position: relative;
   margin-right: .4rem;
   width: 1.4rem;
 `
-const DisplayEmote = styled.div`
+const DisplayEmote = styled(NoSelect)`
   opacity: 1 !important;
   position: absolute;
   top: 0;
   transition: all .2s ease-in-out;
   z-index: 10;
 `
-const EmoteBox = styled.div`
+const EmoteBox = styled(NoSelect)`
   cursor: pointer;
   position: relative;
   font-size: 1.2rem;
@@ -98,16 +106,22 @@ const EmoteBox = styled.div`
   padding: .4rem;
   border-radius: 12px;
   transition: all .2s ease-in-out;
+  
+  @media screen and (max-width: 568px) {
+      font-size: 1.2rem;
+  }
 
   &:hover {
     ${Emote} {
       ${DisplayEmote} {
-        transform: scale(1.4)
+        transform: scale(1.4);
       }
     }
+
     padding-left: 1rem;
     padding-right: 1rem;
   }
+  
   &:active ${Emote} ${DisplayEmote} {
     animation: .4s ease-in-out 0s 1 ${pulse};
   }
