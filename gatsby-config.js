9181264c44d0194fa1b4f93c218039f5efb2e1d5
Gianluca <gianluca@goautonomous.io>
2023-09-27 19:31:49 +0200

    Installed service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,17 +18,20 @@ module.exports = {
     "gatsby-transformer-sharp",
     `gatsby-transformer-remark`,
     `gatsby-transformer-json`,
+    `gatsby-plugin-offline`,
     "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gianluca-belvisi`,
-        short_name: `gbelvisi`,
+        name: `Gianluca Belvisi's Blog`,
+        short_name: `Gianluca Belvisi`,
         start_url: `/`,
-        background_color: `#135291`,
-        theme_color: `#135291`,
+        background_color: `#131313`,
+        theme_color: `#131313`,
         display: `minimal-ui`,
         icon: `src/assets/images/icon.jpg`, // This path is relative to the root of the site.
+        cache_busting_mode: 'query',
+        legacy: true,
       },
     },
     {
